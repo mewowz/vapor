c7fe0474442a9ee1900bddf5621dec3dc5e382c7
mewowz <mewowz@lainx.org>
2026-05-29 18:56:28 -0700

    auth: add integration test for anonymous logon

---
+++ auth_integration_test.go
@@ -0,0 +1,38 @@
+//go:build integration
+
+package vapor
+
+import (
+	"context"
+	"testing"
+	"time"
+)
+
+func TestAnonymousLogon(t *testing.T) {
+	steamConn := NewSteamConnection(10 * time.Second)
+	err := steamConn.CMConnect(10 * time.Second)
+	if err != nil {
+		t.Fatalf("CMConnect failed: %v", err)
+	}
+	defer func() {
+		steamConn.netLoopCancel(context.Canceled)
+		select {
+		case <-steamConn.netLoopCtx.Done():
+		case <-time.After(2 * time.Second):
+			t.Fatalf("netloop: context is not Done()")
+		}
+	}()
+	defer steamConn.conn.Close()
+
+	err = steamConn.StartNetLoop()
+	if err != nil {
+		t.Fatalf("StartNetLoop failed: %v", err)
+	}
+
+	auth := NewAnonymousAuthenticator(steamConn)
+
+	err = auth.Logon()
+	if err != nil {
+		t.Fatalf("auth.Logon failed: %v", err)
+	}
+}
