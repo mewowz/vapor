f0f3ca6de34d2ae5f09c1cd68efb7d8408404566
mewowz <mewowz@lainx.org>
2026-06-09 18:57:41 -0700

    message: implement unified Message interface for all message types
    - Created an interface named Message to represent all message types with
      better scalability when handling arbitary CMsg* types
    - Added extendedMsgHeader
    - Changed field msgHeaderPB.header to a *steamproto.CMsgProtoBufHeader.
      Patched constructors and methods to reflect this change
    - msgHeader, extendedMsgHeader, and msgHeaderPB now all fully implement
      the Message interface
    - Specifically added the Proto method to the Message interface to
      extract the protobuf message within protobuf messages. Non-protobuf
      headers will panic if Proto() is called to enforce type-checking

--- message.go
+++ message.go
@@ -9,29 +9,56 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
+type HeaderType int
+
+const (
+	GenericHeader HeaderType = iota
+	ExtendedHeader
+	ProtoHeader
+)
+
+type Message interface {
+	EMsg() EMsg
+	HeaderType() HeaderType
+	Body() ([]byte, error)
+	Bytes() ([]byte, error)
+	Proto() proto.Message
+}
+
 type connectionHeader struct {
 	PayloadLen uint32
 	Magic      uint32
 }
 
 type msgHeader struct {
-	EMsg        EMsg
-	TargetJobID uint64
-	SourceJobID uint64
-	Body        []byte
+	emsg        EMsg
+	targetJobID uint64
+	sourceJobID uint64
+	body        []byte
+}
+
+type extendedMsgHeader struct {
+	emsg          EMsg
+	headerSize    uint8
+	headerVersion uint16
+	targetJobID   uint64
+	sourceJobID   uint64
+	headerCanary  uint8
+	steamID       uint64
+	sessionID     int32
 }
 
 type msgHeaderPB struct {
-	EMsg   EMsg
-	Header steamproto.CMsgProtoBufHeader
-	Body   proto.Message
+	emsg   EMsg
+	header *steamproto.CMsgProtoBufHeader
+	body   proto.Message
 }
 
 func NewMsgHeaderPB(emsg EMsg, pbBody proto.Message) (*msgHeaderPB, error) {
 	pbHeader := steamproto.CMsgProtoBufHeader{}
 	return &msgHeaderPB{
 		EMsg(uint32(emsg) | 0x80000000),
-		pbHeader,
+		&pbHeader,
 		pbBody,
 	}, nil
 }
@@ -55,7 +82,7 @@ func NewMsgHeaderPBFromBytes(data []byte) (*msgHeaderPB, error) {
 	}
 	return &msgHeaderPB{
 		emsg,
-		pbHeader,
+		&pbHeader,
 		pbBody,
 	}, nil
 }
@@ -97,19 +124,54 @@ func newConnectionHeader(payloadLen uint32) *connectionHeader {
 	}
 }
 
+func (h *msgHeader) EMsg() EMsg {
+	return h.emsg
+}
+
+func (h *msgHeader) HeaderType() HeaderType {
+	return GenericHeader
+}
+
+func (h *msgHeader) Body() ([]byte, error) {
+	var bodyBytes []byte
+	bodyBytes = binary.LittleEndian.AppendUint64(bodyBytes, h.targetJobID)
+	bodyBytes = binary.LittleEndian.AppendUint64(bodyBytes, h.sourceJobID)
+	bodyBytes = append(bodyBytes, h.body...)
+	return bodyBytes, nil
+}
+
+func (h *msgHeader) Proto() proto.Message {
+	panic("call to Proto() on non-protobuf Message")
+}
+
+func (h *msgHeader) Size() int {
+	return int(binary.Size(h.emsg) +
+		binary.Size(h.targetJobID) +
+		binary.Size(h.sourceJobID) +
+		len(h.body))
+}
+
+func (h *msgHeader) Bytes() ([]byte, error) {
+	fullBytes := make([]byte, msgHeaderMinSizeBytes)
+	bodyBytes, _ := h.Body()
+	binary.LittleEndian.PutUint32(fullBytes[:4], uint32(h.emsg))
+	copy(fullBytes[4:], bodyBytes)
+	return fullBytes, nil
+}
+
 // Bytes will return the header in wire-format using Little-Endian encoding.
 // Bytes will also ensure that the EMsg has the correct bit set to indicate that it is
 // a protobuf message.
 func (h *msgHeaderPB) Bytes() ([]byte, error) {
-	headerBytes, err := proto.Marshal(&h.Header)
+	headerBytes, err := proto.Marshal(h.header)
 	if err != nil {
 		return nil, err
 	}
 	var data []byte
-	data = binary.LittleEndian.AppendUint32(data, uint32(h.EMsg)|0x80000000)
+	data = binary.LittleEndian.AppendUint32(data, uint32(h.emsg)|0x80000000)
 	data = binary.LittleEndian.AppendUint32(data, uint32(len(headerBytes)))
 	data = append(data, headerBytes...)
-	bodyBytes, err := proto.Marshal(h.Body)
+	bodyBytes, err := proto.Marshal(h.body)
 	if err != nil {
 		return nil, err
 	}
@@ -117,14 +179,58 @@ func (h *msgHeaderPB) Bytes() ([]byte, error) {
 	return data, nil
 }
 
-func (h *msgHeader) Size() uint {
-	return uint(binary.Size(h.EMsg) +
-		binary.Size(h.TargetJobID) +
-		binary.Size(h.SourceJobID) +
-		len(h.Body))
+func (h *msgHeaderPB) EMsg() EMsg {
+	return h.emsg
 }
 
-func UnpackCMsgMultiToBytes(msg steamproto.CMsgMulti) ([][]byte, error) {
+func (h *msgHeaderPB) HeaderType() HeaderType {
+	return ProtoHeader
+}
+
+func (h *msgHeaderPB) Body() ([]byte, error) {
+	bodyBytes, err := proto.Marshal(h.body)
+	return bodyBytes, err
+}
+
+func (h *msgHeaderPB) Proto() proto.Message {
+	return h.body
+}
+
+func (h *msgHeaderPB) ProtoHeader() *steamproto.CMsgProtoBufHeader {
+	return h.header
+}
+
+func (h *extendedMsgHeader) EMsg() EMsg {
+	return h.emsg
+}
+
+func (h *extendedMsgHeader) HeaderType() HeaderType {
+	return ExtendedHeader
+}
+
+// Body will return all fields byte-encoded from extendedMsgHeader EXCEPT:
+// EMsg, HeaderSize, HeaderVersion
+func (h *extendedMsgHeader) Body() ([]byte, error) {
+	var bodyBytes []byte
+	bodyBytes = binary.LittleEndian.AppendUint64(bodyBytes, h.targetJobID)
+	bodyBytes = binary.LittleEndian.AppendUint64(bodyBytes, h.sourceJobID)
+	bodyBytes = append(bodyBytes, h.headerCanary)
+	bodyBytes = binary.LittleEndian.AppendUint64(bodyBytes, h.steamID)
+	bodyBytes = binary.LittleEndian.AppendUint32(bodyBytes, uint32(h.sessionID))
+	return bodyBytes, nil
+}
+
+func (h *extendedMsgHeader) Bytes() ([]byte, error) {
+	var fullBytes []byte
+	fullBytes = binary.LittleEndian.AppendUint32(fullBytes, uint32(h.emsg))
+	fullBytes = append(fullBytes, h.headerSize)
+	fullBytes = binary.LittleEndian.AppendUint16(fullBytes, h.headerVersion)
+	bodyBytes, _ := h.Body()
+	fullBytes = append(fullBytes, bodyBytes...)
+	return fullBytes, nil
+}
+
+func UnpackCMsgMultiToBytes(msg *steamproto.CMsgMulti) ([][]byte, error) {
 	var rawBody []byte
 	if msg.GetSizeUnzipped() > 0 {
 		decompressed, err := gzipDecompress(msg.GetMessageBody(), msg.GetSizeUnzipped())
@@ -144,6 +250,10 @@ func UnpackCMsgMultiToBytes(msg steamproto.CMsgMulti) ([][]byte, error) {
 		rawBody = rawBody[4+subSize:]
 	}
 	return msgsBytes, nil
+}
+
+func (h *extendedMsgHeader) Proto() proto.Message {
+	panic("call to Proto() on non-protobuf Message")
 }
 
 func gzipDecompress(data []byte, unzippedSize uint32) ([]byte, error) {
@@ -167,15 +277,15 @@ func parseMsgHeader(data []byte) (msgHeader, error) {
 	}
 	var header msgHeader
 
-	header.EMsg = EMsg(binary.LittleEndian.Uint32(data[0:4]))
-	header.TargetJobID = binary.LittleEndian.Uint64(data[4:12])
-	header.SourceJobID = binary.LittleEndian.Uint64(data[12:20])
+	header.emsg = EMsg(binary.LittleEndian.Uint32(data[0:4]))
+	header.targetJobID = binary.LittleEndian.Uint64(data[4:12])
+	header.sourceJobID = binary.LittleEndian.Uint64(data[12:20])
 
 	// Then just read whatever is left into the body, if anything
 	if len(data) > msgHeaderMinSizeBytes {
-		header.Body = data[msgHeaderMinSizeBytes:]
+		header.body = data[msgHeaderMinSizeBytes:]
 	} else {
-		header.Body = []byte{}
+		header.body = []byte{}
 	}
 	return header, nil
 }
