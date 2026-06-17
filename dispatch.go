e26e224f6ab669bb4e7f2e6c29e03fa19a880e7f
mewowz <mewowz@lainx.org>
2026-06-16 21:30:03 -0700

    dispatch: migrate from job-based dispatching to EMsg-based dispatching
    - Added an EMsgListener struct that allows the dispatcher to easily
      return data based on specific EMsg's coming in from the wire
    - Dispatcher now holds all EMsgListeners and a map for EMsg ->
      *EMsgListener
    - Dispatcher now registers listeners given an EMsg and returns an
      *EMsgListener if successful
    - EMsgMulti is now handled and called recursively within DispatchMessage

--- dispatch.go
+++ dispatch.go
@@ -1,21 +1,53 @@
 package vapor
 
 import (
+	"context"
 	"encoding/binary"
+	"errors"
 	"log/slog"
 	"slices"
+	"time"
+
+	"github.com/mewowz/vapor/internal/steamproto"
 )
 
+type EMsgListener struct {
+	emsg       EMsg
+	returnChan chan Message
+	ctx        context.Context
+	ctxCancelF context.CancelFunc
+}
+
 type Dispatcher struct {
-	jobChanMap map[uint64]chan<- Message
-	logger     *slog.Logger
+	emsgChanMap map[EMsg]*EMsgListener
+	listeners   []*EMsgListener
+	logger      *slog.Logger
 }
 
 func NewDispatcher(logger *slog.Logger) *Dispatcher {
 	return &Dispatcher{
-		jobChanMap: make(map[uint64]chan<- Message),
-		logger:     logger,
+		emsgChanMap: make(map[EMsg]*EMsgListener),
+		logger:      logger,
 	}
+}
+
+func NewEMsgListener(emsg EMsg) *EMsgListener {
+	ctx, ctxCancelF := context.WithTimeout(context.Background(), DefaultCMSubmissionTimeout*time.Second)
+	listener := &EMsgListener{
+		emsg:       emsg,
+		returnChan: make(chan Message, 1),
+		ctx:        ctx,
+		ctxCancelF: ctxCancelF,
+	}
+	return listener
+}
+
+func (l *EMsgListener) Read() <-chan Message {
+	return l.returnChan
+}
+
+func (l *EMsgListener) Done() <-chan struct{} {
+	return l.ctx.Done()
 }
 
 func (d *Dispatcher) DispatchMessage(msgBytes []byte) error {
@@ -29,12 +61,17 @@ func (d *Dispatcher) DispatchMessage(msgBytes []byte) error {
 			d.logger.Debug("got error while deserializing ProtoBuf message from bytes", "err", err)
 			return err
 		}
-		//err = d.dispatchPBMessage(message)
-		err = d.writeMessageToCaller(
-			message,
-			message.header.GetJobidTarget(),
-			message.header.GetJobidSource(),
+
+		if emsg == EMsgMulti {
+			err = d.dispatchMulti(message)
+			return err
+		}
+		d.logger.Debug(
+			"got Message to dispatch",
+			"sourceJobID", message.header.GetJobidSource(),
+			"targetJobID", message.header.GetJobidTarget(),
 		)
+		err = d.writeMessageToCaller(message)
 		return err
 	}
 
@@ -49,48 +86,82 @@ func (d *Dispatcher) DispatchMessage(msgBytes []byte) error {
 	} else {
 		message := NewExtendedMsgHeaderFromBytes(msgBytes)
 		//err := d.dispatchExtendedMsg(message)
-		err := d.writeMessageToCaller(
-			message,
-			message.targetJobID,
-			message.sourceJobID,
-		)
+		err := d.writeMessageToCaller(message)
 		return err
 	}
 }
 
-func (d *Dispatcher) Register(jobID uint64, returnChan chan<- Message) error {
-	_, exists := d.jobChanMap[jobID]
-	if exists {
-		return ErrJobIDAlreadyInDispatcher
+func (d *Dispatcher) dispatchMulti(message *msgHeaderPB) error {
+	// For now, I want this to pacnic if the body isn't a CMsgMulti because
+	// that'll mean Steam, for some reason, sent EMsg = 1 without a correct body.
+	// Then, I'll implement some handler, but I'll defer that for now
+	protoBody := message.Proto().(*steamproto.CMsgMulti)
+	unpackedMulti, err := UnpackCMsgMultiToBytes(protoBody)
+	if err != nil {
+		return err
 	}
-	d.jobChanMap[jobID] = returnChan
+	for _, unpackedMessageBytes := range unpackedMulti {
+		err = d.DispatchMessage(unpackedMessageBytes)
+		if errors.Is(err, ErrNoProtoForEMsg) {
+			rawEmsg := EMsg(binary.LittleEndian.Uint32(unpackedMessageBytes[:4]))
+			emsg := rawEmsg & 0x7FFFFFFF
+			d.logger.Debug("no proto handler for EMsg", "EMsg", emsg)
+			continue
+		}
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
-func (d *Dispatcher) writeMessageToCaller(
-	message Message,
-	targetJobID,
-	sourceJobID uint64,
-) error {
-	if targetJobID == DefaultJobID {
-		d.logger.Debug("got DefaultJobID; dropping job", "EMsg", message.EMsg())
-		return nil
+func (d *Dispatcher) Register(emsg EMsg) (*EMsgListener, error) {
+	_, exists := d.emsgChanMap[emsg]
+	if exists {
+		return nil, ErrJobIDAlreadyInDispatcher
 	}
-	jobChan, exists := d.jobChanMap[targetJobID]
+	listener := NewEMsgListener(emsg)
+	d.emsgChanMap[emsg] = listener
+	d.listeners = append(d.listeners, listener)
+	return listener, nil
+}
+
+func (d *Dispatcher) writeMessageToCaller(message Message) error {
+	listener, exists := d.emsgChanMap[message.EMsg()]
 	if !exists {
-		d.logger.Debug(
-			"no mapping for message", "EMsg", message.EMsg(),
-			"sourceJobID", sourceJobID,
-			"targetJobID", targetJobID,
-		)
+		d.logger.Debug("no mapping for message", "EMsg", message.EMsg())
 		return ErrNoChanForJob
 	}
 	select {
-	case jobChan <- message:
+	case listener.returnChan <- message:
 	default:
-		d.logger.Error("blocking on jobChan", "EMsg", message.EMsg(), "targetJobID", targetJobID)
-		panic("jobChan is never allowed to block")
+		d.logger.Error("blocking on returnChan", "EMsg", message.EMsg())
+		panic("returnChan is never allowed to block")
 	}
-	delete(d.jobChanMap, targetJobID)
+	//delete(d.emsgChanMap, message.EMsg())
+	d.removeListener(listener)
 	return nil
+}
+
+func (d *Dispatcher) removeListener(listener *EMsgListener) {
+	for i := range len(d.listeners) {
+		if d.listeners[i].emsg == listener.emsg {
+			d.listeners[i] = d.listeners[len(d.listeners)-1]
+			d.listeners = d.listeners[:len(d.listeners)-1]
+			delete(d.emsgChanMap, listener.emsg)
+			break
+		}
+	}
+}
+
+func (d *Dispatcher) cancelAllListeners() {
+	for _, listener := range d.listeners {
+		listener.cancel()
+	}
+	d.listeners = []*EMsgListener{}
+	d.emsgChanMap = make(map[EMsg]*EMsgListener)
+}
+
+func (l *EMsgListener) cancel() {
+	l.ctxCancelF()
 }
